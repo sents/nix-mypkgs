aac75de227b7de1f4ba41e9a68ffae2da718cc48
Finn Krein <finn@krein.moe>
2023-03-29 19:24:01 +0200

    Remove xwayland
    The security extension landed in nixpkgs, so I don't need a copy in
    mypkgs.

--- overlay.nix
+++ overlay.nix
@@ -5,7 +5,6 @@
       mypkgs = import ./pkgs {pkgs = prev;};
     in
       {
-        xorg = prev.xorg // { xwayland = mypkgs.xwayland;};
         maia-icon-theme = mypkgs.maia-icon-theme;
         mensapy = mypkgs.mensapy;
         swycle = packages.${prev.system}.swycle;
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,5 @@
 {pkgs}:
 {
-  xwayland = pkgs.callPackage ./xwayland {};
   maia-icon-theme = pkgs.libsForQt5.callPackage ./maia-icon-theme {};
   mensapy = pkgs.callPackage ./mensapy {};
 }
