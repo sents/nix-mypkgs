d7018c9926b9e2fc8a94712dc4dc422bad27d56e
Finn Krein <finn@krein.moe>
2023-04-01 16:40:06 +0200

    Make chili theme configurable

--- pkgs/chili-sddm-theme/default.nix
+++ pkgs/chili-sddm-theme/default.nix
@@ -1,9 +1,21 @@
-{lib, stdenv
+{ lib
+, stdenv
 , fetchFromGitHub
 , plasma-framework
 , plasma-workspace
 , kdeclarative
+, config ? {}
 }:
+let
+  configLines = lib.mapAttrsToList (name: value: lib.nameValuePair name value) config;
+  configureTheme = "cp theme.conf theme.conf.orig \n" +
+    (lib.concatMapStringsSep "\n" (configLine:
+      "grep -q '^${configLine.name}=' theme.conf || echo '${configLine.name}=' >> \"$1\"\n" +
+      "sed -i -e 's/^${configLine.name}=.*$/${configLine.name}=${
+        lib.escape [ "/" "&" "\\"] configLine.value
+      }/' theme.conf"
+    ) configLines);
+in
 stdenv.mkDerivation {
   pname = "sddm-chili-theme";
   version = "0.5.5";
@@ -23,15 +35,25 @@ stdenv.mkDerivation {
 
   dontWrapQtApps = true;
 
-  postFixup = ''
-  mkdir -p $out/nix-support
-  echo ${plasma-framework.bin} \
-       ${plasma-workspace} \
-       ${kdeclarative} >> $out/nix-support/propagated-user-env-packages
-  '';
+  preInstall = configureTheme;
 
   postInstall = ''
-  mkdir -p $out/share/sddm/themes/chili
-  mv * $out/share/sddm/themes/chili/
-'';
+    mkdir -p $out/share/sddm/themes/chili
+    mv * $out/share/sddm/themes/chili/
+  '';
+
+  postFixup = ''
+    mkdir -p $out/nix-support
+    echo ${plasma-framework.bin} \
+         ${plasma-workspace} \
+         ${kdeclarative} >> $out/nix-support/propagated-user-env-packages
+  '';
+  meta = with lib; {
+    license = licenses.gpl3;
+    homepage = https://github.com/MarianArlt/sddm-chili;
+    description = "The chili login theme for SDDM";
+    longDescription = ''
+      Chili is hot, just like a real chili! Spice up the login experience for your users, your family and yourself. Chili reduces all the clutter and leaves you with a clean, easy to use, login interface with a modern yet classy touch.
+    '';
+  };
 }
