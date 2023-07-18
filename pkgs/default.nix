f2e7553564f1d35f3c7ddccbdf7c08a955fb858f
Finn Krein <finn@krein.moe>
2023-07-18 23:23:29 +0200

    Add: xdg-terminal-exec

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,5 @@
 {
   mensapy = pkgs.callPackage ./mensapy {};
   sddm-chili-theme = pkgs.libsForQt5.callPackage ./chili-sddm-theme {};
+  xdg-terminal-exec = pkgs.callPackage ./xdg-terminal-exec {};
 }
